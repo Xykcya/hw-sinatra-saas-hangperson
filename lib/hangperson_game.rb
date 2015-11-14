38679e229174a2e36b9b41c9567826cca6f18805
Nikita <justxykc@gmail.com>
2015-11-14 17:48:35 +0000

    gunperson class done

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,88 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
+
+  
+=begin
+функци видимо должна принимать букву - проверки
+проверить есть ли она в слове
+и не было ли попытки её уже отгадать
+=end
+  def guess letter
+      
+    if letter.nil? or letter.length == 0 or not letter=~/[a-z]/i
+      raise ArgumentError
+    end
+    result = false
+    letter.downcase!
+    isLetterAlreadyGuessed = false;
+    isWrongGuessedAlready  = false;
+
+
+
+    
+    if @guesses.include? letter #буква уже была отгадана
+      isLetterAlreadyGuessed = true
+    end
+    if @wrong_guesses.include? letter#была неудачная  попытка угадать эту букву
+      isWrongGuessedAlready = true;
+    end
+    if !isWrongGuessedAlready && !isLetterAlreadyGuessed
+     # puts "word:"
+     # puts @word
+     # puts "letter"
+     # puts letter
+      if @word.include? letter #если слово содержит букву
+    #  puts "yea"
+        @guesses+=letter
+        result = true
+      else
+        result = true
+        @wrong_guesses+=letter
+      end
+    else
+      result = false
+    end
+
+
+    
+    
+    return result
+  end
+ 
+
+def word_with_guesses
+  guessed_word = ""
+  @word.each_char do |letter|
+    if @guesses.include? letter
+      guessed_word << letter
+    else
+      guessed_word << '-'
+    end
+  end
+
+  return guessed_word
+end
+
+def check_win_or_lose
+  return :lose if @wrong_guesses.size>6
+  if word_with_guesses == @word
+    return :win
+  else
+    return :play
+  end
+end
+
+
+#
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   def self.get_random_word
